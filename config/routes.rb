3ba8589d42c177d7393c4c7ba8c4f55339f3f4d0
Long1901 <longuras1901@gmil.com>
2023-01-11 13:44:16 +0700

    add form SignIn/SignUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "home#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "signup", to: "users#new"
+  post "signup", to: "users#create"
 
+  get "signin", to: "sessions#new"
+  post "signin", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
