bfaf7d1ceead6a944863df6c77981a08cb252370
Long1901 <longuras1901@gmil.com>
2023-02-20 09:44:19 +0700

    req#8 times 7

--- db/migrate/20230208045223_add_devise_to_users.rb
+++ db/migrate/20230208045223_add_devise_to_users.rb
@@ -47,10 +47,4 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :role, :integer
     add_column :users, :provider, :string
   end
-
-  def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
-  end
 end
