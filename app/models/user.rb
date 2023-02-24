e91bb4ace9a552a39bf657f534edb53717907900
Long1901 <longuras1901@gmil.com>
2023-02-24 16:11:24 +0700

    update full code

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  before_save { self.email = email.downcase }
+         :omniauthable
 
   validates :name, presence: true, length: { maximum: 50 }
 
-  validates :email, presence: true, uniqueness: true, length: { maximum: 62 }, format: { with: VALID_EMAIL_REGEX }
-
   validates :birthday, presence: true
+
   validates :address, presence: true
 
   enum :role, { user_normal: 0, admin: 1 }
